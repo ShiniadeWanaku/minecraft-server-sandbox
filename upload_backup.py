7c693da4052fca3800c6409ed270fc2e181108cb
ShiniadeWanaku <luisrodrigobarba@outlook.com>
2025-04-02 21:06:05 -0700

    fix(upload_backup.py)!: change paths now relatives to home directory

--- upload_backup.py
+++ upload_backup.py
@@ -1,29 +1,33 @@
+# Import the necessary libraries
 import os
-import zipfile
+import pytz
 import shutil
 import pickle
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import zipfile
+from datetime import datetime
+
+# Import Google Drive API libraries
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from datetime import datetime
-import pytz
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-# Si modificas estos alcances, elimina el archivo token.pickle.
+# Create the scopes for the Google Drive API
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
-# Reemplaza con el ID de tu carpeta en Google Drive
+# Create the folder ID where the files will be uploaded
 FOLDER_ID = '1ZF7l059YxrzfZuxxTKuZbZt6mcsQ4Mit'
 
 def authenticate():
-    """Autenticación para la API de Google Drive."""
+    """Google Drive API authentication."""
     creds = None
-    # El archivo token.pickle guarda el token de acceso.
+    # The file token.pickle stores the user's access and refresh tokens, and is
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
 
-    # Si no hay credenciales disponibles, deja que el usuario inicie sesión.
+    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -31,47 +35,47 @@ def authenticate():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Guarda las credenciales para la próxima vez que se ejecute el script.
+        # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return creds
 
 def compress_folder(folder_path):
-    """Comprime la carpeta especificada en un archivo .zip con un nombre basado en la fecha y hora de Pacific Time."""
-    # Obtener la fecha y hora actual en Pacific Time
+    """Compresse a folder into a zip file with the current date and time in Pacific Time."""
+    # Obtain the current date and time in Pacific Time
     pacific_time = pytz.timezone('US/Pacific')
     current_time = datetime.now(pacific_time)
     
-    # Formatear la fecha y hora en el formato deseado
+    # Format the date and time for the filename
     timestamp = current_time.strftime("%Y-%m-%d_%H-%M-%S")
     
-    # Crear el nombre del archivo comprimido
+    # Create the zip file name
     zip_filename = f"minecraft-server-backup-{timestamp}.zip"
     
-    # Comprimir la carpeta
+    # Compress the folder
     shutil.make_archive(zip_filename.replace('.zip', ''), 'zip', folder_path)
-    print(f'Carpeta comprimida: {zip_filename}')
+    print(f'Zip: {zip_filename}')
     return zip_filename
 
 def upload_file(file_path, mime_type='application/zip'):
-    """Sube un archivo a Google Drive dentro de una carpeta específica."""
+    """Upload a file to Google Drive."""
     creds = authenticate()
     service = build('drive', 'v3', credentials=creds)
 
-    # Crea el archivo que se va a subir
+    # Create the file metadata
     file_metadata = {
         'name': os.path.basename(file_path),
-        'parents': [FOLDER_ID]  # Asignamos el archivo a la carpeta utilizando el ID directamente
+        'parents': [FOLDER_ID]
     }
     media = MediaFileUpload(file_path, mimetype=mime_type)
 
-    # Subir el archivo
+    # Upload the file
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     print(f'Archivo subido con ID: {file["id"]}')
 
 if __name__ == '__main__':
-    # Ruta de la carpeta que deseas comprimir y subir
-    zip_file = compress_folder(os.path.expanduser('~'))
-    # Borrar el archivo .zip después de subirlo
-    os.remove(zip_file)
+    # Path to the folder to be compressed
+    zip_file = compress_folder(os.path.expanduser('~/minecraft-server'))
     upload_file(zip_file)
+    os.remove(os.path.expanduser('~/server-backend/'+ zip_file))
+    
